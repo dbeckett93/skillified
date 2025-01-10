5cc5525fc119c89663f46e44ad7d24914107ef74
Daniel Beckett <dan-beckett@hotmail.co.uk>
2025-01-10 16:26:05 +0000

    Fix WSGI settings module path in wsgi.py

--- skillified/skillified/wsgi.py
+++ skillified/skillified/wsgi.py
@@ -1,6 +1,8 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'skillified.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'skillified.skillified.settings')
 
 application = get_wsgi_application()
+
+import os
