af40bf5a191b7710c38730a3f912c0c3cca08331
Daniel Beckett <dan-beckett@hotmail.co.uk>
2025-01-13 11:32:18 +0000

    Add crispy-bootstrap5 support

--- skillified/settings.py
+++ skillified/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'main',
     # Crispy forms \/
     'crispy_forms',
+    'crispy_bootstrap5',
     # Crispy forms /\
 ]
 
